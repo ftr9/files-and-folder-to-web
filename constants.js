f402d03e0c082d4e75a41aa5d56942a0e0af30fe
ftr9 <ferlandodotel16@gmail.com>
2023-07-30 03:09:52 +0545

    feat: Added Backend url

--- constants.js
+++ constants.js
@@ -1,1 +1,2 @@
-exports.DEFAULT_EXPORTFILE_NAME = 'extractedFilesAndFolder.json';
+export const DEFAULT_EXPORTFILE_NAME = 'extractedFilesAndFolder.json';
+export const BACKEND_URL = `https://files-and-folders-to-web.vercel.app`;
